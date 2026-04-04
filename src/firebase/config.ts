b9dff6807a8d62984e8774c38db5d65663665ea9
Jarif Tahmid <jariftahmid10@gmail.com>
2026-04-04 06:15:01 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,18 +1,8 @@
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyBfVPm2Wu5FvV0Al7H3Z8c5UUpA1R1ZFGg",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-6124962228-d1780.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-6124962228-d1780",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-6124962228-d1780.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "835273246350",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:835273246350:web:0a4939a1f28fa6fe276bdf",
+  "projectId": "studio-6124962228-d1780",
+  "appId": "1:835273246350:web:0a4939a1f28fa6fe276bdf",
+  "apiKey": "AIzaSyBfVPm2Wu5FvV0Al7H3Z8c5UUpA1R1ZFGg",
+  "authDomain": "studio-6124962228-d1780.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "835273246350"
 };
-
-if (typeof window !== 'undefined') {
-  const missingKeys = Object.entries(firebaseConfig)
-    .filter(([_, value]) => !value)
-    .map(([key]) => key);
-
-  if (missingKeys.length > 0) {
-    console.warn(`Missing Firebase configuration keys: ${missingKeys.join(', ')}`);
-  }
-}
